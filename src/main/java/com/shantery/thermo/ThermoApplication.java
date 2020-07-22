0567183f345d620546994f8e326d65880bac60e5
r.kawahara <r.kawahara@NP-21-099-S.shantery.local>
2020-07-22 16:36:30 +0900

    【初期】プロジェクト開始

---
+++ src/main/java/com/shantery/thermo/ThermoApplication.java
@@ -0,0 +1,13 @@
+package com.shantery.thermo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermoApplication.class, args);
+	}
+
+}
