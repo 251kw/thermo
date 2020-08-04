3d19be1b157d33c3ebbd036e058aec30a62782a3
k.takahashi <k.takahashi@NP-22-113-0.shantery.local>
2020-08-04 17:25:28 +0900

    8/4 17:24 80%

--- src/main/java/com/shantery/thermo/ThermoForm.java
+++ src/main/java/com/shantery/thermo/ThermoForm.java
@@ -7,14 +7,14 @@ import javax.validation.constraints.Size;
 public class ThermoForm {
 
 	/** ユーザID **/
-    @NotBlank(message="※スペースは入力できません")
-    @Size(min = 4, max = 32, message="※４～３２文字以内でで入力してください")
+    @NotBlank(message="※空白は入力できません")
+    @Size(min = 4, max = 32, message="※４～３２文字以内で入力してください")
     @Pattern(regexp="[a-zA-Z0-9]*",message="※半角英数字で入力してください")
 	private String userId;
     
 	/** ユーザパスワード **/
-    @NotBlank(message="※スペースは入力できません")
-    @Size(min = 4, max = 16, message="※４～３２文字以内でで入力してください")
+    @NotBlank(message="※空白は入力できません")
+    @Size(min = 4, max = 16, message="※４～３２文字以内で入力してください")
     @Pattern(regexp="[a-zA-Z0-9]*",message="※半角英数字で入力してください")
 	private String userpass;
 
