82ec8b8a9af644a7a1924fcf845bd5f489a972cb
y.okino <y.okino@NP-21-094-0.shantery.local>
2020-08-05 16:11:23 +0900

    コメントの追加、外部化（途中）

--- src/main/java/com/shantery/thermo/userInfoMulti/UserInfoMultiController.java
+++ src/main/java/com/shantery/thermo/userInfoMulti/UserInfoMultiController.java
@@ -30,7 +30,7 @@ import com.shantery.thermo.util.ThermoUtil;
 
 @Controller
 public class UserInfoMultiController {
-	
+
 	@Autowired
 	UserInfoMultiService uMService; //呼び出すクラス
 	@Autowired
@@ -42,7 +42,6 @@ public class UserInfoMultiController {
 	@Autowired
 	HttpSession session; //呼び出すクラス
 
-	
 	/**
 	 * ユーザーを一括登録するファイルをアップロードする画面に遷移させる
 	 * @return
@@ -50,10 +49,10 @@ public class UserInfoMultiController {
 	 */
 	@RequestMapping(value = USERS_MULTI_SET, method = RequestMethod.GET) // アプリケーションを起動させたとき、もしくは会社のロゴが押されたとき
 	public String userInfoInput(Model model) throws ParseException {
-	
+
 		return TO_USERS_MULTI_INP;
 	}
-	
+
 	/**
 	 * アップロードしたファイルを受けとり、解析したのち、
 	 * エラーがなければ確認画面へ、エラーがあればアップロード画面へ戻す
@@ -61,70 +60,86 @@ public class UserInfoMultiController {
 	 * @return
 	 * @throws ParseException
 	 */
-	@RequestMapping(value = USERS_MULTI_GET , method = RequestMethod.POST)
-	public String getCSVfile(@RequestParam(name = USERS_CSV) MultipartFile usersInfo,  Model model) throws ParseException {
+	@RequestMapping(value = USERS_MULTI_GET, method = RequestMethod.POST)
+	public String getCSVfile(@RequestParam(name = USERS_CSV) MultipartFile usersInfo, Model model)
+			throws ParseException {
 		if (session.getAttribute(USERS_INFO_SES) != null) { // もしsessionスコープ内にデータがあるなら削除する
 			session.removeAttribute(USERS_INFO_SES);
 		}
-		String rtn = TO_USERS_MULTI_INP;
-		List<String[]> users = uMService.toStringList(usersInfo);
-		Iterable<GroupMstEntity> glist = gimr.findAll();
+		String rtn;  //遷移先の宣言
+		List<String[]> users = uMService.toStringList(usersInfo); //CSVファイルの解析
+		Iterable<GroupMstEntity> glist = gimr.findAll(); //DB内の
 		Iterable<UserInfoEntity> ulist = uimr.findAll();
 		List<String> errmsg = new ArrayList<>();
-		
-		if(usersInfo.isEmpty()) {
-			errmsg.add("ファイルが選択されていません"); //TODO 日本語の外部化
-		}else if(uMService.checkUserinfo(users)){
+
+		if (usersInfo.isEmpty()) { //ファイルが選択されていない場合
+			errmsg.add((msgPro.getMessage("view.errUserFileExisting", new String[] {}, Locale.JAPAN)));
+		} else if (uMService.checkUserinfo(users)) { //ファイルの形式が正しくない場合
 			errmsg.add("ファイルの配列が正しくありません");
-		}else if(uMService.checkUserId(users)) {
+		} else if (uMService.checkUserId(users)) { //一括登録するユーザーIDに重複がある場合
 			errmsg.add("ログインIdが重複しています");
-		}else {
+		} else {
+			//一括登録するユーザーの数だけ繰り返す
 			for (int i = 0; i < users.size(); i++) {
-				String getmsg = null;
-				getmsg = uMService.loginGroup(glist, users.get(i), i);
-				if(getmsg != null) {
-					errmsg.add(getmsg);
+				String getmsg = null; //エラーメッセージの宣言
+				getmsg = uMService.loginGroup(glist, users.get(i), i); //グループにログインできるかチェック
+				if (getmsg != null) {
+					errmsg.add(getmsg); //nullじゃない場合エラーメッセージをadd
 				}
-				getmsg = null;
-				getmsg = uMService.checUserIdDB(ulist, users.get(i), i);
-				if(getmsg != null) {
-					errmsg.add(getmsg);
+				getmsg = null; //エラーメッセージの初期化
+				getmsg = uMService.checUserIdDB(ulist, users.get(i), i); //DBにログインIDの重複があるか調べる
+				if (getmsg != null) {
+					errmsg.add(getmsg); //nullじゃない場合エラーメッセージをadd
 				}
-				
+
+				//BeanValidation のエラーの受け取り準備
 				ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		        Validator validator = factory.getValidator();
-				
-		        UserInfoMultiBeanValidation uimbv = new UserInfoMultiBeanValidation();
-				
-					uimbv.setAll(users.get(i));
-					  Set<ConstraintViolation<UserInfoMultiBeanValidation>> constraintViolations = validator.validate(uimbv);
-					  int errorCount = constraintViolations.size();
-					  if (errorCount > 0) {
-						  for (ConstraintViolation<UserInfoMultiBeanValidation> violation : constraintViolations) {
-							  errmsg.add((i+1) + "行目：" + violation.getMessage());
-						  }
-				        }
-				
-		        
-		        
+				Validator validator = factory.getValidator();
+
+				//BeanValidation のインスタンス化
+				UserInfoMultiBeanValidation uimbv = new UserInfoMultiBeanValidation();
+
+				//インスタンス化されたBeanにユーザーをset
+				uimbv.setAll(users.get(i));
+
+				//エラーをチェック
+				Set<ConstraintViolation<UserInfoMultiBeanValidation>> constraintViolations = validator.validate(uimbv);
+				int errorCount = constraintViolations.size();
+				//エラーを格納するListの宣言
+				List<String> getVio = new ArrayList<String>();
+				//エラーが検出された場合、エラー仕分けしてエラーメッセージを取得
+				if (errorCount > 0) {
+					//エラーの数繰り返す
+					for (ConstraintViolation<UserInfoMultiBeanValidation> violation : constraintViolations) {
+						getVio.add(violation.getMessage());  //エラーの種類を取得
+					}
+					//エラーメッセージに行数を追加しerrmsgに格納
+					for (String getErr : uMService.makeErrMsg(getVio)) {
+						errmsg.add((i + 1) + "行目の" + getErr);
+					}
+
+				}
+
 			}
-			
+
 		}
-		
-		if (CollectionUtils.isEmpty(errmsg)) {
-			session.setAttribute(USERS_INFO_SES, users);
-			model.addAttribute(USERS_HEAD, ThermoUtil.getColumnName(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));
-			model.addAttribute(USERS_HEAD_LENG, ThermoUtil.getColumnCount(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));
-			model.addAttribute(USERS_INFO, users);
-			rtn = TO_USERS_MULTI_CONF;
-		}else {
-			model.addAttribute(USERS_ERR, errmsg);
-			rtn = TO_USERS_MULTI_INP;
+
+		if (CollectionUtils.isEmpty(errmsg)) { //エラーメッセージがない場合確認画面に
+			session.setAttribute(USERS_INFO_SES, users);  //ユーザー情報をセッションにいれる
+			model.addAttribute(USERS_HEAD,
+					ThermoUtil.getColumnName(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));  //ヘッドをmodelにいれる
+			model.addAttribute(USERS_HEAD_LENG,
+					ThermoUtil.getColumnCount(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));  //ヘッドの長さをmodelに入れる TODO 不必要の可能性があるので削除予定
+			model.addAttribute(USERS_INFO, users);  //ユーザー情報をmodelにいれる
+			rtn = TO_USERS_MULTI_CONF;  //遷移先を確認画面に指定
+		} else {  //エラーメッセージがある場合
+			model.addAttribute(USERS_ERR, errmsg);  //エラーメッセージをmodelに入れる
+			rtn = TO_USERS_MULTI_INP;  //遷移先を入力（アップロード画面）に指定
 		}
-		
-		return rtn ;
+
+		return rtn;
 	}
-	
+
 	/**
 	 * 確認画面で確定を押された時に、データベースに追加して、結果画面に
 	 * @return
@@ -133,19 +148,21 @@ public class UserInfoMultiController {
 	@RequestMapping(value = USERS_MULTI_CONF_SUC, method = RequestMethod.POST)
 	//@Transactional
 	public String userInfoConfirm(Model model) throws ParseException {
-		model.addAttribute(USERS_HEAD, ThermoUtil.getColumnName(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));
-		model.addAttribute(USERS_HEAD_LENG, ThermoUtil.getColumnCount(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));
-		@SuppressWarnings("unchecked")
-		List<String[]> users = (List<String[]>)session.getAttribute(USERS_INFO_SES);
-		model.addAttribute(USERS_INFO, users);
-		UserInfoEntity uEntity = new UserInfoEntity();
-		for (String[] userInfo : users) {
-			uEntity.setUserInfo(userInfo);
-			uimr.save(uEntity);
+		model.addAttribute(USERS_HEAD,
+				ThermoUtil.getColumnName(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN))); //ヘッドをmodelにいれる
+		model.addAttribute(USERS_HEAD_LENG,
+				ThermoUtil.getColumnCount(msgPro.getMessage("view.usercolumns", new String[] {}, Locale.JAPAN)));  //ヘッドの長さをmodelに入れる TODO 不必要の可能性があるので削除予定
+		@SuppressWarnings("unchecked")  //未検査のキャストをするため  TODO 動作の安全確認
+		List<String[]> users = (List<String[]>) session.getAttribute(USERS_INFO_SES);  //セッションから登録するユーザー情報を取得
+		model.addAttribute(USERS_INFO, users);  //modelに登録するユーザー情報をいれる
+		//TODO サービスクラスへの移行
+		UserInfoEntity uEntity = new UserInfoEntity();  //DBに登録するエンティティクラスのインスタンス化
+		for (String[] userInfo : users) {  //登録するユーザー情報の数だけ繰り返す
+			uEntity.setUserInfo(userInfo);  //ユーザー情報をset
+			uimr.save(uEntity);  //DBに登録
 		}
-		
-		
-		return TO_USERS_MULTI_RES ;
+
+		return TO_USERS_MULTI_RES;
 	}
-	
+
 }
--- src/main/java/com/shantery/thermo/userInfoMulti/UserInfoMultiService.java
+++ src/main/java/com/shantery/thermo/userInfoMulti/UserInfoMultiService.java
@@ -19,37 +19,47 @@ import com.shantery.thermo.entity.UserInfoEntity;
 public class UserInfoMultiService {
 	
 	/**
-	 * @param usersInfo ユーザー情報の入ったＣＳＶファイル
+	 * @param usersInfo 登録ユーザー情報の入ったＣＳＶファイル
 	 * @return カンマで区切られた情報の配列
 	 * @throws ParseException
 	 */
 	public List<String[]> toStringList(MultipartFile usersInfo) throws ParseException {
 		
+		//戻り値に設定する変数の宣言
 		List<String[]> lines = new ArrayList<String[]>();
+		//分割数の宣言
         String division = null;
         try {
+        	//CSVファイルの解析
             InputStream stream = usersInfo.getInputStream();           
             Reader reader = new InputStreamReader(stream, "Shift_JIS");
             BufferedReader buf= new BufferedReader(reader);
+            //解析したファイルをコンマで分割
             while((division = buf.readLine()) != null) {
             	String[] line = division.split(",", 0);
+            	//戻り値に分割した情報をadd
             	lines.add(line);
             }
-            division = buf.readLine();
 
-        } catch (IOException e) {
+        } catch (IOException e) { //解析中にエラーを検出した場合nullを返す
             lines = null;
         }
         return lines;
 		
 	}
 	
+	/**
+	 * CSVファイルの形式を調べる
+	 * @param usersInfo ユーザー情報
+	 * @return T/F CSVファイルの形式が正しくなければ T
+	 */
 	public boolean checkUserinfo(List<String[]> usersInfo) {
 		boolean bool = false;
 		
+		//一括登録するユーザーの長さ繰り返す
 		for (int i = 0; i < usersInfo.size(); i++) {
-			String[] line = usersInfo.get(i);
-			if(line.length != 9) {
+			//カラム数が9じゃなかった場合TRUEに
+			if(usersInfo.get(i).length != 9) {
 				bool = true;
 				break;
 			}
@@ -59,11 +69,18 @@ public class UserInfoMultiService {
 		return bool;
 	}
 	
+	/**
+	 * 登録ユーザーIDの重複チェック
+	 * @param usersInfo 登録ユーザー情報
+	 * @return T/F 登録ユーザー情報に重複があれば T
+	 */
 	public boolean checkUserId(List<String[]> usersInfo) {
 		boolean bool = false;
 		
+		//一括登録するユーザーの長さ繰り返す
 		for (int i = 0; i < usersInfo.size(); i++) {
 			String[] line = usersInfo.get(i);
+			//1行目と二行目以降にログインIDに重複がないか調べる、その後ループに戻ると2行目と3行目以降を調べる..を繰り返す
 			for(int j = i + 1;j < usersInfo.size(); j++) {
 				String[] ckline = usersInfo.get(j);
 				if(line[2].equals(ckline[2])) {
@@ -76,6 +93,13 @@ public class UserInfoMultiService {
 		return bool;
 	}
 	
+	/**
+	 * ログインの可否のチェックとエラーメッセージの排出
+	 * @param glist DB内の情報
+	 * @param usersInfo 登録ユーザー情報
+	 * @param i 行数
+	 * @return エラーメッセージ
+	 */
 	public String loginGroup(Iterable<GroupMstEntity> glist ,String[] usersInfo,int i) {
 		boolean bool = true;
 		String errmsg = null;
@@ -105,6 +129,83 @@ public class UserInfoMultiService {
 			if(list.getUser_id().equals(usersInfo[2])) { 
 				errmsg = ((i+1) + "行目のユーザーIdは既に使われています");
 			}
+		}
+		
+		return errmsg;
+	}
+	
+	/**
+	 * バリデーションビーンからのエラーを解析して画面に表示させるエラーメッセージを返す
+	 * @param vios バリデーションビーンから送られてきたエラーメッセージ
+	 * @return エラーメッセージ
+	 */
+	public List<String> makeErrMsg(List<String> vios){
+		
+		//エアラーメッセージのリストの宣言
+		List<String> errmsg = new ArrayList<String>();
+		
+		//調べる受け取ったエラー該当する場合にaddするための変数の宣言
+		String gId = null;
+		String gpass = null;
+		String uId = null;
+		String uPass = null;
+		String uName = null;
+		String gender = null;
+		String birthday = null;
+		String grade = null;
+		String flg = null;
+		
+		//エラーの仕分けとメッセージの格納
+		for (String vio : vios) {
+			if(vio.equals("gId")) {
+				gId = "グループID：4文字以上、32文字以下の半角英数字で入力してください";
+			}else if(vio.equals("gpass")) {
+				gpass = "グループパスワード：4文字以上、16文字以下の半角英数字で入力してください";
+			}else if(vio.equals("uId")) {
+				uId = "ユーザーID：4文字以上、32文字以下の半角英数字で入力してください";
+			}else if(vio.equals("uPass")) {
+				uPass = "ユーザーパスワード：4文字以上、16文字以下の半角英数字で入力してください";
+			}else if(vio.equals("uName")) {
+				uName = "ユーザー名：1文字以上、64文字以下の記号以外の文字で入力してください";
+			}else if(vio.equals("gender")) {
+				gender = "性別：Ｍ(男性)またはF(女性)で入力してください";
+			}else if(vio.equals("birthday")) {
+				birthday = "生年月日：　yyyyMMdd　の形式で入力してください";
+			}else if(vio.equals("grade")) {
+				grade = "学年：数値で入力してください";
+			}else if(vio.equals("flg")) {
+				flg = "管理者権限：数値で入力してください";
+			}
+			
+		}
+		
+		//nullでない場合のみadd
+		if(gId != null) {
+			errmsg.add(gId);
+		}
+		if(gpass != null) {
+			errmsg.add(gpass);
+		}
+		if(uId != null) {
+			errmsg.add(uId);
+		}
+		if(uPass != null) {
+			errmsg.add(uPass);
+		}
+		if(uName != null) {
+			errmsg.add(uName);
+		}
+		if(gender != null) {
+			errmsg.add(gender);
+		}
+		if(birthday != null) {
+			errmsg.add(birthday);
+		}
+		if(grade != null) {
+			errmsg.add(grade);
+		}
+		if(flg != null) {
+			errmsg.add(flg);
 		}
 		
 		return errmsg;
