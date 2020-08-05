986b10571d6841f68952a13090a25b753309a1b9
y.sato <y.sato@NP-22-112-0.shantery.local>
2020-08-05 09:21:53 +0900

    検索機能　サービス、フォーム

---
+++ src/main/java/com/shantery/thermo/search/SearchInfoForm.java
@@ -0,0 +1,34 @@
+package com.shantery.thermo.search;
+
+
+/*
+ * 検索条件form
+ */
+public class SearchInfoForm {
+
+	private String sch_date;
+	private String sch_name;
+	private String sch_grade;
+	
+	
+	public String getSch_date() {
+		return sch_date;
+	}
+	public void setSch_date(String sch_date) {
+		this.sch_date = sch_date;
+	}
+	public String getSch_name() {
+		return sch_name;
+	}
+	public void setSch_name(String sch_name) {
+		this.sch_name = sch_name;
+	}
+	public String getSch_grade() {
+		return sch_grade;
+	}
+	public void setSch_grade(String sch_grade) {
+		this.sch_grade = sch_grade;
+	}
+
+	
+}
