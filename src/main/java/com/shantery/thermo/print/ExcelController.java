4a907afa312a14990987d057dc6d2806ed0b34b6
d.ito <d.ito@NP-21-098-0.shantery.local>
2020-08-05 17:31:07 +0900

    【印刷機能】searchEntity排除

--- src/main/java/com/shantery/thermo/print/ExcelController.java
+++ src/main/java/com/shantery/thermo/print/ExcelController.java
@@ -1,4 +1,3 @@
-
 package com.shantery.thermo.print;
  
 import java.util.List;
@@ -8,37 +7,32 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.shantery.thermo.entity.ThermoInfoEntity;
-import com.shantery.thermo.entity.UserInfoEntity;
-import com.shantery.thermo.search.SearchEntity;
-  
+
 @Controller 
 public class ExcelController {
 	
 	//リポジトリクラス呼出し
-//	@Autowired
-//	ExcelRepository repository;
-//	@Autowired
-//	ExcelRepository2 repository2;
+	@Autowired
+	ExcelRepository repository;
 	
 	@Autowired
 	HttpSession session; //呼び出すクラス
 	
-//	@RequestMapping("/ex")
-//	public String exsample(Model model) {
-//		model.addAttribute("msg", "sample");
-//		List<ThermoEntity> list = repository.findAll();
-//		model.addAttribute("list", list);
-//		session.setAttribute("samplelist",list);
-//		return "extest";
-//	}
+	@RequestMapping("/excel")
+	public String exsample(Model model) {
+		model.addAttribute("msg", "sample");
+		List<ThermoInfoEntity> list = repository.findAll();
+		model.addAttribute("list", list);
+		session.setAttribute("samplelist",list);
+
+		return "extest";
+	}
 	
-	@RequestMapping("/excel") 
-	public  ExcelView excel( ExcelView mav) {
+	@RequestMapping("/ex") 
+	public  ExcelView excel(ExcelView mav) {
 		return mav; 
 	} 
 }
--- src/main/java/com/shantery/thermo/print/ExcelView.java
+++ src/main/java/com/shantery/thermo/print/ExcelView.java
@@ -1,6 +1,4 @@
 package com.shantery.thermo.print;
-
-
 import java.util.List;
 import java.util.Map;
 
@@ -8,57 +6,72 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import org.apache.poi.hssf.usermodel.HeaderFooter;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Footer;
 import org.apache.poi.ss.usermodel.Header;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
 import com.shantery.thermo.entity.ThermoInfoEntity;
-import com.shantery.thermo.search.SearchEntity;
-
+/**
+ * @author d.ito
+ *検索結果をExcelに 
+ */
 @Component
 public class ExcelView extends AbstractXlsxView {
-
+	public String flagCheck(String flag) {
+		String text = null;
+		if(flag.equals("0")) {
+			text = "なし";
+		}else {
+			text = "あり";
+		}
+		return text;
+	}
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		// セッションに接続
 		HttpSession session = request.getSession();
-
 		// 検索結果をリストで取得
-		List<SearchEntity> list = (List<SearchEntity>) session.getAttribute("samplelist");
-		// シート作成
-		Sheet sheet = workbook.createSheet("検温情報");
+		List<ThermoInfoEntity> list = (List<ThermoInfoEntity>) session.getAttribute("samplelist");	
+		Sheet sheet = workbook.createSheet("検温情報"); // シート作成
 		// 検温表の項目を配列化
-		String[] rowTmp = {"日付", "名前", "性別", "学年", "年齢", "体温", "味覚障害", "嗅覚障害", "咳", "その他" };
-
- 		sheet.setColumnWidth(2, 1150);
- 		sheet.setColumnWidth(6, 1150);
- 		sheet.setColumnWidth(7, 1150);
- 		sheet.setColumnWidth(8, 1150);
- 		sheet.setColumnWidth(9, 5500);
+		String[] rowTmp = {"計測日", "名前", "性別", "学年", "年齢", "体温", "味覚障害", "嗅覚障害", "咳", "その他" };
+		//列の幅を設定する
+		sheet.setColumnWidth(0, 2800); //計測日の幅
+ 		sheet.setColumnWidth(2, 1150); //性別の幅
+ 		sheet.setColumnWidth(4, 1150); //年齢の幅
+ 		sheet.setColumnWidth(5, 1150); //体温の幅
+ 		sheet.setColumnWidth(6, 1150); //味覚障害の幅
+ 		sheet.setColumnWidth(7, 1150); //嗅覚障害の幅
+ 		sheet.setColumnWidth(8, 1150); //咳の幅
+ 		sheet.setColumnWidth(9, 5500); //その他の幅
 		// セルスタイルを指定
 		CellStyle cellstyle = workbook.createCellStyle();
-
-		int rowNum = 0;
-		Row row = sheet.createRow(rowNum++);
 		
-		//ヘッダー作成
-		Header header = sheet.getHeader();
+		int rowNum = 0;
+		String text = null;
+		Row row = sheet.createRow(rowNum++);
+		Cell cell = null;	
+		
+		Header header = sheet.getHeader(); //ヘッダーの作成
 		header.setCenter("検温情報");
+		header.setLeft(HeaderFooter.date()); //日付の指定
+	    Footer footer = sheet.getFooter(); //フッターの作成
+	    footer.setCenter(HeaderFooter.page() + "/" + HeaderFooter.numPages());
 		
 		for (int i = 0; i <= list.size(); i++) {
 			if (i == 0) {
 				for (int j = 0; j < rowTmp.length; j++) {
-					Cell cell = row.createCell(j);
+					cell = row.createCell(j);
 					cell.setCellValue(rowTmp[j]);
 					cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 					cellstyle.setBorderRight(BorderStyle.MEDIUM);
@@ -70,7 +83,7 @@ public class ExcelView extends AbstractXlsxView {
 				//改行を行う
 				row = sheet.createRow(rowNum++);
 				//1列目に日付を挿入
-				Cell cell = row.createCell(0);
+				cell = row.createCell(0);
 				cell.setCellValue(list.get(i - 1).getRegist_date());
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
@@ -79,7 +92,7 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//2列目に名前を挿入
 				cell = row.createCell(1);
-				cell.setCellValue(list.get(i - 1).getUser_name());
+				cell.setCellValue(list.get(i - 1).getCough());
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
@@ -87,7 +100,7 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//3列目に性別を挿入
 				cell = row.createCell(2);
-				cell.setCellValue(list.get(i - 1).getGender());
+				cell.setCellValue(list.get(i - 1).getThermo());
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
@@ -95,7 +108,7 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//4列目に学年を挿入
 				cell = row.createCell(3);
-				cell.setCellValue(list.get(i - 1).getGrade());
+				cell.setCellValue(list.get(i - 1).getOlfactory_disorder());
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
@@ -103,7 +116,7 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//5列目に年齢を挿入
 				cell = row.createCell(4);
-				cell.setCellValue(list.get(i - 1).getAge());
+				cell.setCellValue(list.get(i - 1).getThermo_id());
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
@@ -119,7 +132,12 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//7列目に味覚障害を挿入
 				cell = row.createCell(6);
-				cell.setCellValue(list.get(i - 1).getTaste_disorder());
+				if(list.get(i - 1).getTaste_disorder().equals("0")) {
+					text = "なし";
+				}else {
+					text = "あり";
+				}
+				cell.setCellValue(text);
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
@@ -127,7 +145,8 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//8列目に嗅覚障害を挿入
 				cell = row.createCell(7);
-				cell.setCellValue(list.get(i - 1).getOlfactory_disorder());
+				cell.setCellValue(flagCheck(list.get(i - 1).getTaste_disorder()));
+				cell.setCellValue(text);
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
@@ -135,7 +154,12 @@ public class ExcelView extends AbstractXlsxView {
 				cell.setCellStyle(cellstyle);
 				//9列目に咳を挿入
 				cell = row.createCell(8);
-				cell.setCellValue(list.get(i - 1).getCough());
+				if(list.get(i - 1).getCough().equals("0")) {
+					text = "なし";
+				}else {
+					text = "あり";
+				}
+				cell.setCellValue(text);
 				cellstyle.setBorderLeft(BorderStyle.MEDIUM);
 				cellstyle.setBorderRight(BorderStyle.MEDIUM);
 				cellstyle.setBorderTop(BorderStyle.MEDIUM);
