8854e2d3ba8254350624f5bbaf4628ff477c9cae
t.kurihara <t.kurihara@NP-22-109-0.shantery.local>
2020-08-05 09:57:16 +0900

    ThermoInput機能

---
+++ src/main/java/com/shantery/thermo/thermoInput/ThermoInputController.java
@@ -0,0 +1,128 @@
+package com.shantery.thermo.thermoInput;
+
+
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shantery.thermo.entity.ThermoInfoEntity;
+import com.shantery.thermo.thermoInput.ThermoInputService;
+
+import antlr.collections.List;
+
+@Controller
+class ThermoInputController {
+	
+	@Autowired
+	HttpSession session;
+	@Autowired
+	ThermoInputService service;
+	@Autowired
+	ThermoInputRepository repository;
+	
+	/**
+	 * テスト
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		Iterable<ThermoInfoEntity> list = repository.findAll();
+		mav.addObject("date",list);
+		return mav;
+	}
+
+	/**
+	 * 検温情報入力ボタンから入力画面へ
+	 * @param page
+	 * @param model
+	 * @return
+	 * @throws ParseException
+	 */
+	@RequestMapping(value = "/from_search"/*FROM_SEARCH_BUTTON*/ , method = RequestMethod.GET)
+	public String Input() {
+		/*検索画面 からくる　多分佐藤が作る*/
+		/*model にグループIdが同じの人をもらう*/
+		return "thermoInput"/*TO_INPUT*/;
+	}
+	
+	/**
+	 * 検温情報入力から確認画面へ
+	 * @param page
+	 * @param model
+	 * @return
+	 * @throws ParseException
+	 */
+	@RequestMapping(value = "/from_input"/*FROM_INPUT_BUTTON*/ , method = RequestMethod.POST)
+	public String InputConfirm(@ModelAttribute("thForm") ThermoInputForm form, Model model) {
+		
+		model.addAttribute("list", form.gettList());
+		session.setAttribute("list", form.gettList());
+		//サービスで入力チェックメソッド入れる,エラーを出す
+		String msg = null;
+		
+		if(msg != null) {
+			model.addAttribute("msg",msg);
+			return "thermoInput";
+		}
+		
+		return "thermoConfirm"/*TO_INPUT_CONFIRM*/;
+	}
+	
+	/**
+	 * 検温情報確認画面から結果画面へ
+	 * @param page
+	 * @param model
+	 * @return
+	 * @throws ParseException
+	 */
+	@RequestMapping(value =  "/from_confirm"/*FROM_INPUT_CONFIRM_BUTTON*/ , method = RequestMethod.POST)
+	public String InputResult() {
+		ArrayList<ThermoInputForm.Detail> list = (ArrayList<ThermoInputForm.Detail>)session.getAttribute("list");
+		
+		service.registAll(list);
+		
+		
+		return "thermoResult"/*TO_INPUT_RESULT*/;
+	}
+	
+	/**
+	 * 登録情報入力画面へ戻る
+	 * @param page
+	 * @param model
+	 * @return
+	 * @throws ParseException
+	 */
+	@RequestMapping(value = "/return_input/"/*FROM_RETURN_INPUT_BUTTON*/ , method = RequestMethod.POST)
+	public String InputReturn() {
+		
+		
+		return "thermoInput" /*TO_INPUT*/;
+	}
+	
+	/**
+	 * 検索画面へ戻る
+	 * @param page
+	 * @param model
+	 * @return
+	 * @throws ParseException
+	 */
+	@RequestMapping(value = "/return_search/"/*FROM_RETURN_SEARCH_BUTTON*/ , method = RequestMethod.GET)
+	public String SearchReturn() {
+		
+		
+		return "search"/*TO_SEARCH*/;
+	}
+	
+}
---
+++ src/main/java/com/shantery/thermo/thermoInput/ThermoInputForm.java
@@ -0,0 +1,115 @@
+package com.shantery.thermo.thermoInput;
+
+
+import java.util.ArrayList;
+
+
+public class ThermoInputForm {
+	
+	private ArrayList<Detail> tList;
+	
+	public ArrayList<Detail> gettList() {
+		return tList;
+	}
+
+	public void settList(ArrayList<Detail> tList) {
+		this.tList = tList;
+	}
+
+	public static class Detail {
+	
+		
+		/* ユーザー名 */
+		private String userName;
+		/* 性別 */
+		private String gender;
+		/* 学年 */
+		private String grade;
+		/* 年齢 */
+		private String age;
+		/* ユーザーID */
+		private String userId;
+		/* 体温 */
+		private String temperature;
+		/* 味覚障害 */
+		private String taste;
+		/* 嗅覚障害 */
+		private String smell;
+		/* 咳 */
+		private String cough;
+		/* その他 */
+		private String writing;
+		/* 登録日 */
+		private String date;
+		
+		
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public String getGrade() {
+			return grade;
+		}
+		public void setGrade(String grade) {
+			this.grade = grade;
+		}
+		public String getAge() {
+			return age;
+		}
+		public void setAge(String age) {
+			this.age = age;
+		}
+		public String getUserId() {
+			return userId;
+		}
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+		public String getTemperature() {
+			return temperature;
+		}
+		public void setTemperature(String temperature) {
+			this.temperature = temperature;
+		}
+		public String getTaste() {
+			return taste;
+		}
+		public void setTaste(String taste) {
+			this.taste = taste;
+		}
+		public String getSmell() {
+			return smell;
+		}
+		public void setSmell(String smell) {
+			this.smell = smell;
+		}
+		public String getCough() {
+			return cough;
+		}
+		public void setCough(String cough) {
+			this.cough = cough;
+		}
+		public String getWriting() {
+			return writing;
+		}
+		public void setWriting(String writing) {
+			this.writing = writing;
+		}
+		public String getDate() {
+			return date;
+		}
+		public void setDate(String date) {
+			this.date = date;
+		}
+	
+	}
+	
+}
