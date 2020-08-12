bb6e83f7ec13817746110c32e2b80c7374963395
y.okino <y.okino@NP-21-094-0.shantery.local>
2020-08-12 09:32:30 +0900

    コメントの追加

--- src/main/java/com/shantery/thermo/userInfoMulti/GroupInfoMultiRepository.java
+++ src/main/java/com/shantery/thermo/userInfoMulti/GroupInfoMultiRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.shantery.thermo.entity.GroupMstEntity;
 
+/**
+ * @author y.okino
+ *sqlを実行し、データ登録,取得を行う
+ */
 @Repository
 public interface GroupInfoMultiRepository extends JpaRepository<GroupMstEntity, Long> {
 
--- src/main/java/com/shantery/thermo/userInfoMulti/UserInfoMultiRepository.java
+++ src/main/java/com/shantery/thermo/userInfoMulti/UserInfoMultiRepository.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.shantery.thermo.entity.UserInfoEntity;
 
+/**
+ * @author y.okino
+ *sqlを実行し、データ登録,取得を行う
+ */
 @Repository
 public interface UserInfoMultiRepository extends JpaRepository<UserInfoEntity, Long> {
 
-	
 	
 }
