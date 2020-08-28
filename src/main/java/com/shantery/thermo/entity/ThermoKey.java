372784b281420e3792cf7958c22c992316edd89b
t.kurihara <t.kurihara@NP-22-109-0.shantery.local>
2020-08-28 14:22:10 +0900

    コメント削除

--- src/main/java/com/shantery/thermo/entity/ThermoKey.java
+++ src/main/java/com/shantery/thermo/entity/ThermoKey.java
@@ -8,38 +8,4 @@ public class ThermoKey implements Serializable {
 	private String userId;
 	private String thermoId;
 	
-	
-	//TODO 不具合起きなかったら下記メソッド消す
-	/**@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((thermo_id == null) ? 0 : thermo_id.hashCode());
-		result = prime * result + ((user_id == null) ? 0 : user_id.hashCode());
-		return result;
-	}
-
-    @Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ThermoKey other = (ThermoKey) obj;
-		if (thermo_id == null) {
-			if (other.thermo_id != null)
-				return false;
-		} else if (!thermo_id.equals(other.thermo_id))
-			return false;
-		if (user_id == null) {
-			if (other.user_id != null)
-				return false;
-		} else if (!user_id.equals(other.user_id))
-			return false;
-		return true;
-	}
-	**/
-	
 }
