edc1c153d01e321cfcbc76944310cbb375211459
y.sato <y.sato@NP-22-112-0.shantery.local>
2020-08-31 10:37:35 +0900

    html,scroll追加

---
+++ src/main/resources/static/js/scroll.js
@@ -0,0 +1,11 @@
+$(function(){
+	var topBtn=$('#pageTop');
+	 
+	// ◇ボタンをクリックしたら、スクロールして上に戻る
+	topBtn.click(function(){
+	  $('body,html').animate({
+	  scrollTop: 0},500);
+	  return false;
+	});
+
+});
