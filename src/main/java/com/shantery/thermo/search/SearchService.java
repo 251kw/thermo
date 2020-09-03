145cb4c19561f66ea4383ecbfd921d4833823cb9
t.kurihara <t.kurihara@NP-22-109-0.shantery.local>
2020-09-03 13:40:40 +0900

    ulist調整追加

--- src/main/java/com/shantery/thermo/search/SearchService.java
+++ src/main/java/com/shantery/thermo/search/SearchService.java
@@ -102,4 +102,17 @@ public class SearchService {
 		return result;
 	}
 	
+	public List<UserInfoEntity> userlistCheck (List<UserInfoEntity> ulist,SearchInfoForm form){
+		if(EMPTY.equals(form.getSch_date())) {
+			int count = ulist.size();
+			for(int i=0; i+1<count; i++) {
+				for(int t=0; t<13; t++) {
+					ulist.add(i*14, ulist.get(i));
+				}
+			}
+		}
+		
+		return ulist;
+	}
+	
 }
