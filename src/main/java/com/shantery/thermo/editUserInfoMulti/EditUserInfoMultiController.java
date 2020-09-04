aec124a2dd87d9692ee1a32864f57afd7687b605
k.takahashi <k.takahashi@NP-22-113-0.shantery.local>
2020-09-04 17:02:37 +0900

    9/4 17:01 複数編集の機能のみ完了

--- src/main/java/com/shantery/thermo/editUserInfoMulti/EditUserInfoMultiController.java
+++ src/main/java/com/shantery/thermo/editUserInfoMulti/EditUserInfoMultiController.java
@@ -2,17 +2,11 @@ package com.shantery.thermo.editUserInfoMulti;
 
 
 import static com.shantery.thermo.util.ThermoConstants.LOGIN_USER;
-import static com.shantery.thermo.util.ThermoConstants.THERMO_FORM;
-
 import java.util.ArrayList;
-import java.util.Optional;
-
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,36 +26,74 @@ class EditUserInfoMultiController {
 	@Autowired
 	HttpSession session; 
 	
+	// 初期処理
 	@RequestMapping(value = "/editusersmultiset", method = RequestMethod.GET)
-	public String storeInfo(
-			UserInfoEntity userinfo,
-			Model model){
+	public String storeInfo(){
 
-		EditUserInfoMultiForm userlist = new EditUserInfoMultiForm();
-		ArrayList<UserInfoEntity> original = new ArrayList<UserInfoEntity>();
-		//Optional<UserInfoEntity> data = null;
+		// sessionを空にする
+		if (session.getAttribute("userlist") != null) {
+			session.removeAttribute("userlist");
+		}
+		if (session.getAttribute("originallist") != null) {
+			session.removeAttribute("originallist");
+		}
+		
+		//EditUserInfoMultiForm form = new EditUserInfoMultiForm();
+		ArrayList<UserInfoEntity> original = new ArrayList<>();
+		ArrayList<EditUserInfoMultiForm.contents> formlist = new ArrayList<>();
 		UserInfoEntity loginuser = (UserInfoEntity)session.getAttribute(LOGIN_USER);
 		
-		original = service.storeOriginalInfo(loginuser.getGroup_id(),userinfo,original);
+		original = service.storeOriginalInfo(loginuser.getGroup_id(),original);
 		
-		userlist.settList(original);
+		formlist = service.makeFormList(formlist, original);
 		
-		model.addAttribute("userlist", userlist.gettList());
+		
+		session.setAttribute("userlist", formlist);
+		session.setAttribute("originallist", formlist);
 
 		// ユーザー情報一括変更画面へ移動
 		return "editUserInfoMultiInput";
 	}	
 		
-		
+	// 入力から確認ボタン
 	@RequestMapping(value = "/editusersmulticonfirm", method = RequestMethod.POST)
 	public String receiveInfo(
-			@ModelAttribute("userlist") EditUserInfoMultiForm userlist,
-			BindingResult result,
+			@ModelAttribute("userlist") EditUserInfoMultiForm form,
 			Model model){
+
+		ArrayList<EditUserInfoMultiForm.contents> originallist;
+		originallist = (ArrayList<EditUserInfoMultiForm.contents>)session.getAttribute("originallist");
+		ArrayList<EditUserInfoMultiForm.contents> updatelist = new ArrayList<>();
+		updatelist = service.makeUpdateList(updatelist, form.getUserList(), originallist);
 		
-		model.addAttribute("userlist", userlist.gettList());
+		session.setAttribute("userlist", form.getUserList());
+		session.setAttribute("updatelist", updatelist);
 
 		// ユーザー情報一括変更画面へ移動
+		return "editUserInfoMultiConfirm";
+	}
+	
+	// 確認画面から戻る
+	@RequestMapping(value = "/editusersmultireturn", method = RequestMethod.GET)
+	public String returnInfo(){
+		
+		// ユーザー情報一括変更画面へ移動
+		session.removeAttribute("updatelist");
 		return "editUserInfoMultiInput";
+	}
+	
+	// 確認画面から結果画面
+	@RequestMapping(value = "/editusersmultiresult", method = RequestMethod.POST)
+	public String updateInfo(@ModelAttribute("userlist") EditUserInfoMultiForm form){
+		
+		UserInfoEntity loginuser = (UserInfoEntity)session.getAttribute(LOGIN_USER);
+		ArrayList<UserInfoEntity> registlist = new ArrayList<>();
+		
+		registlist = service.makeRegistList(form.getUserList(), loginuser.getGroup_id());
+		
+		service.registInfo(registlist);
+		
+		// ユーザー情報一括変更結果画面へ移動
+		return "editUserInfoMultiResult";
 	}
 }
--- src/main/java/com/shantery/thermo/editUserInfoMulti/EditUserInfoMultiForm.java
+++ src/main/java/com/shantery/thermo/editUserInfoMulti/EditUserInfoMultiForm.java
@@ -2,22 +2,93 @@ package com.shantery.thermo.editUserInfoMulti;
 
 import java.util.ArrayList;
 
-
-import com.shantery.thermo.entity.UserInfoEntity;
+import javax.persistence.Column;
+import javax.persistence.Id;
 
 
 
 public class EditUserInfoMultiForm {
 
 	
-	private ArrayList<UserInfoEntity> userList;
-	
-	public ArrayList<UserInfoEntity> gettList() {
+	private ArrayList<contents> userList;
+
+	public ArrayList<contents> getUserList() {
 		return userList;
 	}
 
-	public void settList(ArrayList<UserInfoEntity> userList) {
+	public void setUserList(ArrayList<contents> userList) {
 		this.userList = userList;
 	}
-
+	
+	
+	public static class contents {
+		
+			
+		@Id
+		/** ユーザID **/
+		@Column(length=32,name="user_id")
+		private String userId;
+		/** ユーザパスワード **/
+		@Column(length=16,name="user_pass")
+		private String userPass;
+		/** 氏名 **/
+		@Column(length=64,name="user_name")
+		private String userName;
+		/** 性別 **/
+		@Column(length=1)
+		private String gender;
+		/** 生年月日 **/
+		@Column
+		private String birthday;
+		/** 学年区分 **/
+		@Column(length=1)
+		private String grade;
+		/** 管理者フラグ **/
+		@Column(length=1,name="admin_flg")
+		private String adminFlg;
+		
+		
+		public String getUser_id() {
+			return userId;
+		}
+		public void setUser_id(String userId) {
+			this.userId = userId;
+		}
+		public String getUser_pass() {
+			return userPass;
+		}
+		public void setUser_pass(String userPass) {
+			this.userPass = userPass;
+		}
+		public String getUser_name() {
+			return userName;
+		}
+		public void setUser_name(String userName) {
+			this.userName = userName;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public String getBirthday() {
+			return birthday;
+		}
+		public void setBirthday(String birthday) {
+			this.birthday = birthday;
+		}
+		public String getGrade() {
+			return grade;
+		}
+		public void setGrade(String grade) {
+			this.grade = grade;
+		}
+		public String getAdmin_flg() {
+			return adminFlg;
+		}
+		public void setAdmin_flg(String adminFlg) {
+			this.adminFlg = adminFlg;
+		}
+	}
 }
