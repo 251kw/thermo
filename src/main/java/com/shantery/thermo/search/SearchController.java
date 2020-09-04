cf1a5e88320bb244ad93f19a52d624be535ce36b
y.sato <y.sato@NP-22-112-0.shantery.local>
2020-09-04 09:41:07 +0900

    search戻し

--- src/main/java/com/shantery/thermo/search/SearchController.java
+++ src/main/java/com/shantery/thermo/search/SearchController.java
@@ -1,8 +1,6 @@
 package com.shantery.thermo.search;
 
 import static com.shantery.thermo.util.ThermoConstants.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -17,12 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.shantery.thermo.ThermoForm;
-//import com.shantery.thermo.entity.ThermoInfoEntity;
+import com.shantery.thermo.entity.ThermoInfoEntity;
 import com.shantery.thermo.entity.UserInfoEntity;
-import com.shantery.thermo.thermoInput.ThermoInputForm;
-import com.shantery.thermo.thermoInput.ThermoInputService;
-import com.shantery.thermo.thermoInput.ThermoInputUserRepository;
-import com.shantery.thermo.util.ThermoReplaceValue;
 
 /**
  * @author y.sato
@@ -35,14 +29,8 @@ class SearchController {
 	@Autowired
 	private SearchService schService; //サービスクラス呼び出す
 	
-//	@Autowired
-//	private SearchRepository schRepository;	//リポジトリクラス呼び出す
-	
 	@Autowired
-	ThermoInputUserRepository thmInUserRepository; //
-	
-	@Autowired
-	ThermoInputService thmInService; //
+	private SearchRepository schRepository;	//リポジトリクラス呼び出す
 	
 	@Autowired
 	HttpSession session; 
@@ -55,52 +43,25 @@ class SearchController {
 	@RequestMapping(value =SEARCH_RETURN, method = RequestMethod.GET) //検索画面に来た時(戻る)
 	public  String search(Model  m){
 		UserInfoEntity loginuser = (UserInfoEntity)session.getAttribute(LOGIN_USER);
-		
-		//グループIDからとってくるに変更
-		Iterable<UserInfoEntity> ulist = thmInUserRepository.findByGroupIdOrderByUpdateTime(loginuser.getGroup_id());
-		
-		
-		//ログインユーザーたちが今日すでに登録しているか確認
-		ArrayList<ThermoInputForm.Detail> list = thmInService.checkRegistDate(ulist);
-		
-		
-		//データベースからとってきたユーザー情報を表示用に変換する 
-		ArrayList<String> birth = new ArrayList<String>();
-		for(UserInfoEntity ul : ulist) {
-			ul.setGender(ThermoReplaceValue.valueToName(KBN_TYPE_GENDER, ul.getGender()));
-			ul.setGrade(ThermoReplaceValue.valueToName(KBN_TYPE_GRADE, ul.getGrade()));
-			birth.add(ThermoReplaceValue.calcAge(ul.getBirthday())+"歳");	
-		}
-				
-		boolean display = true;
+		List<ThermoInfoEntity> list = schRepository.searchCurDate(loginuser.getGroup_id());	//今日の日付で検索	//group_idで絞る
+		boolean display = true;		//テーブルを表示させるか
 		
 		boolean adminbtn = schService.isAdminFlg(loginuser);	//管理者フラグがあれば検温ボタンを押せる
 		
 		m.addAttribute("searchInfo", new SearchInfoForm());
-		m.addAttribute(THERMO_LIST ,list);
-		m.addAttribute(THERMO_USER_LIST, ulist);
-		m.addAttribute(THERMO_BIRTH, birth);
-		m.addAttribute(THERMO_INP_FORM, new ThermoInputForm());
-		session.setAttribute(THERMO_USER_LIST, ulist);
-		session.setAttribute(THERMO_BIRTH, birth);
+		m.addAttribute("list", list);
 		
-		/*if(list.size()==0) {	//今日の検温情報がないとき
+		if(list.size()==0) {	//今日の検温情報がないとき
 			m.addAttribute("nolist_msg", TODAY_NOLIST_MSG);
 			display = false;
 			
-		}*/
-		
-		if(list.size()==MAX_SCH_LISTINT){ //検温情報が最大件数に達したとき
+		} else if(list.size()==MAX_SCH_LISTINT){ //検温情報が最大件数に達したとき
 			m.addAttribute("overlist_msg", OVER_LIST_MSG);
 		}
 		m.addAttribute("display", display);
 		m.addAttribute("adminbtn", adminbtn);
 		
 		session.setAttribute(SCH_LIST, list);		//印刷用
-		
-		session.removeAttribute("uslist");
-		session.removeAttribute("uForm");
-
 		
 		return TO_SEARCH;
 	}
@@ -115,7 +76,7 @@ class SearchController {
 	public String search_info(@ModelAttribute("searchInfo") @Validated SearchInfoForm form, BindingResult result, Model m) {
 		
 		UserInfoEntity loginuser = (UserInfoEntity)session.getAttribute(LOGIN_USER);
-		List<UserInfoEntity> ulist = null;
+		List<ThermoInfoEntity> list = null;
 		boolean display = true;		//テーブルを表示させるか
 		boolean errorFlg = false;
 		
@@ -138,84 +99,27 @@ class SearchController {
 		
 		//errorがなければ検索
 		if(!errorFlg) {
-			ulist = schService.userSearch(loginuser.getGroup_id(), form);
+			list = schService.separate(loginuser.getGroup_id(), form);
 		} else {
-//			list = (List<ThermoInfoEntity>) session.getAttribute(SCH_LIST);
+			list = (List<ThermoInfoEntity>) session.getAttribute(SCH_LIST);
 		}
 		
-		if(ulist.size()==0) {	//検索結果がないとき
+		if(list.size()==0) {	//検索結果がないとき
 			m.addAttribute("nolist_msg", NOLIST_MSG);
 			display = false;
-		} else if(ulist.size()==MAX_SCH_LISTINT){ //検温情報が最大件数に達したとき
+		} else if(list.size()==MAX_SCH_LISTINT){ //検温情報が最大件数に達したとき
 			m.addAttribute("overlist_msg", OVER_LIST_MSG);
 		}
 		
-		
-		ulist= schService.userlistCheck(ulist, form);
-		
-		//ulistを元に検温情報があるかチェック
-		ArrayList<ThermoInputForm.Detail> list = thmInService.checkRegistDate(ulist);
-		
-		//データベースからとってきたユーザー情報を表示用に変換する 
-		ArrayList<String> birth = new ArrayList<String>();
-		for(UserInfoEntity ul : ulist) {
-			ul.setGender(ThermoReplaceValue.valueToName(KBN_TYPE_GENDER, ul.getGender()));
-			ul.setGrade(ThermoReplaceValue.valueToName(KBN_TYPE_GRADE, ul.getGrade()));
-			birth.add(ThermoReplaceValue.calcAge(ul.getBirthday())+"歳");	
-		}
-				
-		
-		m.addAttribute(THERMO_BIRTH, birth);
-		m.addAttribute(THERMO_INP_FORM, new ThermoInputForm());
 		m.addAttribute("searchInfo", form);
+		m.addAttribute("list", list);
 		m.addAttribute("display", display);
 		m.addAttribute("adminbtn", adminbtn);
 		
-		m.addAttribute(THERMO_LIST ,list);
-		m.addAttribute(THERMO_USER_LIST, ulist);
-		
-		session.setAttribute(SCH_LIST, ulist);	//印刷用
+		session.setAttribute(SCH_LIST, list);	//印刷用
 	
 		return TO_SEARCH;
 	}
-	
-	
-	/**
-	 * 更新ボタンが押された時に
-	 * 起動するメソッド
-	 * @param form
-	 * @param result
-	 * @param model
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value = THERMO_INFO_INP_SUC, method = RequestMethod.POST)
-	public String InputConfirm(@Validated @ModelAttribute(THERMO_INP_FORM) ThermoInputForm form, BindingResult result, Model model) {
-		
-		//入力情報を取得
-		ArrayList<ThermoInputForm.Detail> list = form.gettList();
-		model.addAttribute(THERMO_LIST , list);
-		session.setAttribute(THERMO_LIST , list);
-		model.addAttribute(THERMO_USER_LIST, (Iterable<UserInfoEntity>)session.getAttribute(THERMO_USER_LIST));
-		model.addAttribute(THERMO_BIRTH, (ArrayList<String>)session.getAttribute(THERMO_BIRTH));
-		boolean display = true;
-		model.addAttribute("display", display);
-		model.addAttribute("searchInfo", new SearchInfoForm());
-		
-		
-		//入力チェックをしてエラー文があれば入力画面へ
-		ArrayList<String> message = thmInService.checkInput(list);
-		if(message.contains(THERMO_INP_ER) || message.contains(THERMO_INP_TEMP_ER) || result.hasErrors()) {
-			model.addAttribute("message", message);
-			return TO_THERMO_INFO_INP;
-		}
-		
-		//登録実行
-		thmInService.registAll(list);
-		
-		return TO_SEARCH;
-	}
-	
 	
 	/**
 	 * ログアウトボタンが押されたときに起動する
--- src/main/java/com/shantery/thermo/search/SearchRepositoryImp.java
+++ src/main/java/com/shantery/thermo/search/SearchRepositoryImp.java
@@ -1,5 +1,7 @@
 package com.shantery.thermo.search;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,8 +12,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.shantery.thermo.entity.UserInfoEntity;
-
+import com.shantery.thermo.entity.ThermoInfoEntity;
 import static com.shantery.thermo.util.ThermoConstants.*;
 /**
  * @author y.sato
@@ -23,81 +24,81 @@ public class SearchRepositoryImp implements SearchRepositoryCustom {
 	@Autowired
 	HttpSession session; 
 	
-	@PersistenceContext
-	EntityManager entityManager;
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	//TODO 現状、全角英数字と半角英数字を区別してしまう。
-	public List<UserInfoEntity> searchQuery(String groupId, SearchInfoForm form) {
-		
-		StringBuilder sql = new StringBuilder();
-		
-//		boolean dateFlg = false;
-		boolean nameFlg = false;
-		boolean gradeFlg = false;
-		
-		sql.append("SELECT u ");
-		sql.append("FROM UserInfoEntity u ");
-		sql.append("WHERE u.groupId = :groupId ");
-		
-		
-//		if(!EMPTY.equals(form.getSch_date())) {			//日付が入力されたら
-//			sql.append("AND t.registDate = :date ");
-//			dateFlg = true;
-//			
-//			if(!EMPTY.equals(form.getSch_name())) {		//名前が入力されたら、文字列の空白は削除
-//				sql.append("AND REPLACE(REPLACE(t.userInfoEntity.userName,' ', ''), '　', '') LIKE :name ");
-//				nameFlg = true;
-//			}
-//			if(!EMPTY.equals(form.getSch_grade())) {		//学年が入力されたら
-//				sql.append("AND t.userInfoEntity.grade = :grade ");
-//				gradeFlg = true;
-//			}
-//		} else {
+    @PersistenceContext
+    EntityManager entityManager;
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    //TODO 現状、全角英数字と半角英数字を区別してしまう。
+    public List<ThermoInfoEntity> searchQuery(String groupId, SearchInfoForm form) {
+    	
+    	StringBuilder sql = new StringBuilder();
+    	
+    	boolean dateFlg = false;
+    	boolean nameFlg = false;
+    	boolean gradeFlg = false;
+    	
+    	sql.append("SELECT t ");
+    	sql.append("FROM ThermoInfoEntity t ");
+    	sql.append("WHERE t.userInfoEntity.groupId = :groupId ");
+    	
+    	
+    	if(!EMPTY.equals(form.getSch_date())) {			//日付が入力されたら
+    		sql.append("AND t.registDate = :date ");
+    		dateFlg = true;
+    		
+    		if(!EMPTY.equals(form.getSch_name())) {		//名前が入力されたら、文字列の空白は削除
+    			sql.append("AND REPLACE(REPLACE(t.userInfoEntity.userName,' ', ''), '　', '') LIKE :name ");
+    			nameFlg = true;
+    		}
+    		if(!EMPTY.equals(form.getSch_grade())) {		//学年が入力されたら
+    			sql.append("AND t.userInfoEntity.grade = :grade ");
+    			gradeFlg = true;
+    		}
+    	} else {
+    		
+    		if(!EMPTY.equals(form.getSch_name())) {
+    			sql.append("AND REPLACE(REPLACE(t.userInfoEntity.userName,' ', ''), '　', '') LIKE :name ");
+    			nameFlg = true;
+    			
+    			if(!EMPTY.equals(form.getSch_grade())) {
+        			sql.append("AND t.userInfoEntity.grade = :grade ");
+        			gradeFlg = true;
+        		}
+    		}
+    		
+    		if(!EMPTY.equals(form.getSch_grade())) {
+    			sql.append("AND t.userInfoEntity.grade = :grade ");
+    			gradeFlg = true;
+    		}
+    	}
+    	
+    	if(!dateFlg) { //日付指定がなかったら、二週間分を設定
+			sql.append("AND t.registDate BETWEEN :endDate AND :curDate ");
+    		sql.append("order by t.registDate desc, t.userInfoEntity.grade, t.userInfoEntity.userName ");	//日付、学年、名前順で並べる
+	    } else {
+	    	sql.append("order by t.userInfoEntity.grade, t.userInfoEntity.userName ");
+	    }
+    	
+    	Query query = entityManager.createQuery(sql.toString());
+    	
+    	query.setParameter("groupId", groupId);
+    	
+    	//以下、フラグがtrueの時に値をセット
+		if (dateFlg) {
+			query.setParameter("date", form.getSch_date());
+		} else {
+			Calendar cal = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
 			
-			if(!EMPTY.equals(form.getSch_name())) {
-				sql.append("AND REPLACE(REPLACE(u.userName,' ', ''), '　', '') LIKE :name ");
-				nameFlg = true;
-				
-				if(!EMPTY.equals(form.getSch_grade())) {
-					sql.append("AND u.grade = :grade ");
-					gradeFlg = true;
-				}
-			}
+			String curDate = sdf.format(cal.getTime());		//今日の日付
 			
-			if(!EMPTY.equals(form.getSch_grade())) {
-				sql.append("AND u.grade = :grade ");
-				gradeFlg = true;
-			}
-//		}
-		
-//		if(!dateFlg) { //日付指定がなかったら、二週間分を設定
-//			sql.append("AND t.registDate BETWEEN :endDate AND :curDate ");
-//			sql.append("order by t.registDate desc, t.userInfoEntity.grade, t.userInfoEntity.userName ");	//日付、学年、名前順で並べる
-//		} else {
-			sql.append("order by u.grade, u.userName ");
-//		}
-		
-		Query query = entityManager.createQuery(sql.toString());
-		
-		query.setParameter("groupId", groupId);
-		
-		//以下、フラグがtrueの時に値をセット
-//		if (dateFlg) {
-//			query.setParameter("date", form.getSch_date());
-//		} else {
-//			Calendar cal = Calendar.getInstance();
-//			SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
-//			
-//			String curDate = sdf.format(cal.getTime());		//今日の日付
-//			
-//			cal.add(Calendar.DAY_OF_MONTH, -13);			//日付計算
-//			String endDate = sdf.format(cal.getTime());		//二週間前の日付
-//			
-//			query.setParameter("curDate", curDate);
-//			query.setParameter("endDate", endDate);
-//		}
+			cal.add(Calendar.DAY_OF_MONTH, -13);			//日付計算
+			String endDate = sdf.format(cal.getTime());		//二週間前の日付
+			
+			query.setParameter("curDate", curDate);
+			query.setParameter("endDate", endDate);
+		}
 		if (nameFlg) {	//入力された文字列の空白も削除
 			String rename = form.getSch_name().replaceAll("　", " ").replaceAll(" ", "");
 			query.setParameter("name", Q_PERCENT+rename+Q_PERCENT);
