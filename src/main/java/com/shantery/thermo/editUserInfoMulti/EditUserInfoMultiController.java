a6175d2d3ed8b1d880821addcdc85770019e4ce7
r.kawahara <r.kawahara@NP-21-099-S.shantery.local>
2020-10-28 09:00:33 +0900

    2020/10/28 障害対応No.75、No.76
    EditUserInfoMultiController.java
    →ログインユーザーの変更フラグを削除
    ※結果画面からの戻り先を一括変更画面から検温検索画面に一律変更した為
    editGroupInfoResult.html
    editUserInfoResult.html
    →戻るボタンをリンクからサブミットに変更
    editUserInfoMultiDeleteResult.html
    editUserInfoMultiResult.html
    →障害対応
    ※結果画面からの戻り先を一括変更画面から検温検索画面に一律変更した為
    thermoResult.html
    →不要なＵＲＬパラメーターを削除

--- src/main/java/com/shantery/thermo/editUserInfoMulti/EditUserInfoMultiController.java
+++ src/main/java/com/shantery/thermo/editUserInfoMulti/EditUserInfoMultiController.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,16 +33,16 @@ class EditUserInfoMultiController {
 
 	@Autowired
 	private EditUserInfoMultiService service;
-	
+
 	@Autowired
 	EditUserInfoMultiRepository repository;
-	
+
 	@Autowired
 	EditUserInfoMultiRepositoryThermo t_repository;
-	
+
 	@Autowired
-	HttpSession session; 
-	
+	HttpSession session;
+
 	/**
 	 * 検索画面から遷移してきた時の処理
 	 * @param model
@@ -67,19 +68,19 @@ class EditUserInfoMultiController {
 		if (session.getAttribute("deluserList") != null) {
 			session.removeAttribute("deluserList");
 		}
-		
+
 		ArrayList<UserInfoEntity> original = new ArrayList<>();
 		ArrayList<EditUserInfoMultiForm.contents> formlist = new ArrayList<>();
-		
+
 		// ログイン中のユーザー情報を取得
 		UserInfoEntity loginuser = (UserInfoEntity)session.getAttribute(LOGIN_USER);
-		
+
 		// 初期表示用のデータを取得
 		original = service.storeOriginalInfo(loginuser.getGroup_id(),original);
-		
+
 		// 取得したデータをformに詰め替える
 		formlist = service.makeFormList(formlist, original);
-		
+
 		// データを格納
 		model.addAttribute("userlist", formlist);		// 表示用
 		session.setAttribute("originallist", formlist);	// 比較用
@@ -107,19 +108,19 @@ class EditUserInfoMultiController {
 
 		// 入力画面の情報を全て取得
 		inputlist = form.getUserList();
-		
+
 		// ２つのリストを比較して変更されたユーザーのみを取得
 		updatelist = service.makeUpdateList(updatelist, inputlist, originallist);
-		
+
 		//チェックされたユーザーの情報保持
 		String[] deluser = form.getInputMultiCheck();
 		String errormsg = null;
 		ArrayList<UserInfoEntity> delUserList = new ArrayList<>();
 		//削除されるユーザーの有無を確認
 		if(deluser == null || deluser.length == 0) {
-			errormsg = "チェックボックスを入力してください";	
+			errormsg = "チェックボックスを入力してください";
 			// データを格納
-			model.addAttribute("userlist", form.getUserList());	
+			model.addAttribute("userlist", form.getUserList());
 			model.addAttribute("errormsg",errormsg);
 			//削除されるユーザーを選択していない場合Input画面へ遷移
 			return TO_EDIT_USER_INFO_MULTI_INP;
@@ -137,7 +138,7 @@ class EditUserInfoMultiController {
 			return TO_EDIT_USER_INFO_MULTI_DEL_COMF;
 		}
 	}
-	
+
 	/**
 	 * 削除完了画面に遷移
 	 * ユーザーとその検温情報を削除する
@@ -168,7 +169,7 @@ class EditUserInfoMultiController {
 		model.addAttribute("flag", flag);
 		return TO_EDIT_USER_INFO_MULTI_DEL_RES;
 	}
-	
+
 	/**
 	 * 削除確認画面から戻るが押された場合
 	 * @param model
@@ -178,18 +179,18 @@ class EditUserInfoMultiController {
 	@RequestMapping(value = EDIT_USER_INFO_MULTI_DEL_REZ, params= EDIT_USER_INFO_MULTI_DEL_BACK, method = RequestMethod.POST)
 	public String returnuerdel(
 			Model model,EditUserInfoMultiForm form){
-		
+
 		// 値保持のために入力情報が全て格納されているリストをバインド
 		model.addAttribute("userlist", form.getUserList());
-		
+
 		// 役割のなくなったリストをセッションから削除
 		session.removeAttribute("updatelist");
 		session.removeAttribute("inputlist");
 
 		// ユーザー情報一括変更画面へ移動
 		return TO_EDIT_USER_INFO_MULTI_INP;
-	}	
-	
+	}
+
 	/**
 	 * ログインユーザーを削除しログイン画面に遷移する場合
 	 * @return
@@ -197,12 +198,12 @@ class EditUserInfoMultiController {
 	@RequestMapping(value = EDIT_USER_INFO_MULTI_SET,params = EDIT_USER_INFO_MULTI_DEL_LOGOUT, method = RequestMethod.GET)
 	public String backlogin(@ModelAttribute(THERMO_FORM) ThermoForm FormValue) {
 		//session一括削除
-		session.invalidate();	
-		
+		session.invalidate();
+
 		// トップページへ移動
 		return TO_TOP;
 	}
-	
+
 	/**
 	 * ユーザー更新確認画面へ遷移する
 	 * @param form
@@ -222,7 +223,7 @@ class EditUserInfoMultiController {
 		ArrayList<EditUserInfoMultiForm.contents> originallist;
 		ArrayList<EditUserInfoMultiForm.contents> updatelist = new ArrayList<>();
 		ArrayList<EditUserInfoMultiForm.contents> inputlist = new ArrayList<>();
-		
+
 		// セッションから比較用リストを取得
 		originallist = (ArrayList<EditUserInfoMultiForm.contents>)session.getAttribute("originallist");
 		for (int i = 0; i <= form.getUserList().size() - 1; i++) {
@@ -230,42 +231,42 @@ class EditUserInfoMultiController {
 		}
 		// 入力画面の情報を全て取得
 		inputlist = form.getUserList();
-			
+
 		// ２つのリストを比較して変更されたユーザーのみを取得
 		updatelist = service.makeUpdateList(updatelist, inputlist, originallist);
-			
+
 		// updatelistの中身がある場合
 		if(!(updatelist.isEmpty())) {
-			
+
 			// 変更箇所を入力チェックして、エラーメッセージリストを作成
 			errlist = service.checkInputList(inputlist, errlist);
-			
+
 			// errlistのnull部分を排除した結果、空のリストができたなら値は正常
 			nullcheck = service.checkNull(errlist, nullcheck);
-			
+
 			// errlistが空でなかったのなら入力画面に戻す
 			if(nullcheck==false) {
 				transition = TO_EDIT_USER_INFO_MULTI_INP;
 				model.addAttribute("errlist", errlist);
-				
+
 			// errlistが空なら確認画面へ移動
 			}else {
 				transition = TO_EDIT_USER_INFO_MULTI_COMF;
 			}
-			
+
 		// updatelistの中身がない場合
 		}else {
 			// そのまま確認画面へ
 			transition = TO_EDIT_USER_INFO_MULTI_COMF;
-		}	
+		}
 		model.addAttribute("form",form);
 		model.addAttribute("userlist", form.getUserList());
 		session.setAttribute("inputlist", inputlist);
-		session.setAttribute("updatelist", updatelist);	
+		session.setAttribute("updatelist", updatelist);
 		// 画面移動
 		return transition;
 	}
-	
+
 	/**
 	 * 確認画面の戻るボタンを押された時の処理
 	 * @param model
@@ -274,18 +275,18 @@ class EditUserInfoMultiController {
 	@RequestMapping(value = EDIT_USER_INFO_MULTI_REZ,params=EDIT_USER_INFO_MULTI_CONF_RETURN, method = RequestMethod.POST)
 	public String returnInfo(
 			Model model,EditUserInfoMultiForm form){
-		
+
 		// 値保持のために入力情報が全て格納されているリストをバインド
 		model.addAttribute("userlist", session.getAttribute("inputlist"));
-		
+
 		// 役割のなくなったリストをセッションから削除
 		session.removeAttribute("updatelist");
 		session.removeAttribute("inputlist");
-		
+
 		// ユーザー情報一括入力画面へ移動
 		return TO_EDIT_USER_INFO_MULTI_INP;
 	}
-	
+
 	/**
 	 * 確認画面の変更ボタンを押された時の処理
 	 * @param form
@@ -293,30 +294,24 @@ class EditUserInfoMultiController {
 	 */
 	@RequestMapping(value = EDIT_USER_INFO_MULTI_REZ, method = RequestMethod.POST)
 	public String updateInfo(@ModelAttribute("userlist") EditUserInfoMultiForm form, Model model){
-		
+
 		ArrayList<UserInfoEntity> registlist = new ArrayList<>();
-		//ログインユーザーの管理者権限を判定
-		boolean adminflag = true;
-		
+
 		// ログイン中のユーザー情報を取得
 		UserInfoEntity loginuser = (UserInfoEntity)session.getAttribute(LOGIN_USER);
-		
+
 		// データベースの形式にリストを整形
 		registlist = service.makeRegistList(form.getUserList(), loginuser.getGroup_id());
-		
+
 		// データベースを更新
 		service.registInfo(registlist);
-		
+
 		// ログイン中のユーザー情報が変更されている可能性があるため、再取得
 		Optional<UserInfoEntity> data = repository.findById(loginuser.getUser_id());
 		UserInfoEntity userinfo = data.get();
-		//ログインユーザーが管理者でない場合
-		if(userinfo.getAdmin_flg().equals("0")) {
-			adminflag = false;
-		}
-		model.addAttribute("flag",adminflag);
+
 		session.setAttribute(LOGIN_USER, userinfo);
-		
+
 		// ユーザー情報一括変更結果画面へ移動
 		return TO_EDIT_USER_INFO_MULTI_RES;
 	}
